7fc1711b9ff10b26a202f80accdb370344df9de2
dangthanhphuc2512 <dangthanhphuc2512@gmail.com>
2024-12-25 11:04:36 +0700

    Basic achetructure for project

---
+++ src/app/components/cosmetic/cosmetic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cosmetic',
+  standalone: true,
+  imports: [],
+  templateUrl: './cosmetic.component.html',
+  styleUrl: './cosmetic.component.scss'
+})
+export class CosmeticComponent {
+
+}
---
+++ src/app/components/persional-information/persional-information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persional-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './persional-information.component.html',
+  styleUrl: './persional-information.component.scss'
+})
+export class PersionalInformationComponent {
+
+}
