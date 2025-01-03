ce89951d8431e7751a035d5d4d312ce8105abf59
dangthanhphuc2512 <dangthanhphuc2512@gmail.com>
2025-01-03 13:38:22 +0700

    Config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {provideHttpClient, withFetch} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding()), 
-    provideClientHydration()
+    provideRouter(
+      routes,
+      withComponentInputBinding(), // Liên kết dữ liệu route với Input Binding
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always' // Cho phép binding dữ liệu của router từ component cha xuống component con có thế sửu dụng được
+      })
+    ),
+    provideClientHydration(),
+    provideHttpClient(
+      withFetch()
+    )
   ]
 };
