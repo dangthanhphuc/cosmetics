72016b100f6a76ed84600bb812b3eb1ccc24618b
NguyenSang <nguyentansangit2411@gmail.com>
2025-01-03 17:47:33 +0700

    update-homepage

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 
@@ -6,12 +7,24 @@ import { FooterComponent } from '../footer/footer.component';
   selector: 'app-homepage',
   standalone: true,
   imports: [
+    CommonModule,
     HeaderComponent,
     FooterComponent
   ],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
+  categories = [
+    { name: 'Sữa dưỡng thể', image: 'assets/categories/sua-duong-the.png' },
+    { name: 'Dầu gội', image: 'assets/categories/dau-goi.png' },
+    { name: 'Mặt nạ', image: 'assets/categories/mat-na.png' },
+    { name: 'Nước tẩy trang', image: 'assets/categories/nuoc-tay-trang.png' },
+    { name: 'Sữa rửa mặt', image: 'assets/categories/sua-rua-mat.png' },
+    { name: 'Trang điểm mặt', image: 'assets/categories/trang-diem-mat.png' },
+    { name: 'Trang điểm môi', image: 'assets/categories/trang-diem-moi.png' },
+    { name: 'Trang điểm mắt', image: 'assets/categories/trang-diem-vung-mat.png' }
+  ];
 
+  
 }
