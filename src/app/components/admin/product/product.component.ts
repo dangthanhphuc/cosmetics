18f09248bf4ef3e0c6df6fa5c9d3b1f1722eef0f
NguyenSang <nguyentansangit2411@gmail.com>
2025-01-05 13:59:48 +0700

    page-product

--- src/app/components/admin/product/product.component.ts
+++ src/app/components/admin/product/product.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Thêm CommonModule vào đây
   templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+  styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
+  products = [
+    { id: 1, name: 'Sản phẩm 1', price: 500000 },
+    { id: 2, name: 'Sản phẩm 2', price: 300000 },
+    { id: 3, name: 'Sản phẩm 3', price: 300000}
+  ];
 
+  addProduct() {
+    
+  }
+
+  viewProduct(productId: number) {
+    
+  }
+
+  editProduct(productId: number) {
+    
+  }
+
+  deleteProduct(productId: number) {
+    
+  }
 }
