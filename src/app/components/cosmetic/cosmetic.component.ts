0621494019236c522482eb9682b203378dbf29cc
dangthanhphuc2512 <dangthanhphuc2512@gmail.com>
2025-01-05 19:38:26 +0700

    The card-item, cosmetic, homepage components

--- src/app/components/cosmetic/cosmetic.component.ts
+++ src/app/components/cosmetic/cosmetic.component.ts
@@ -1,23 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Import CommonModule
+import { UUID } from 'crypto';
+import { UserService } from '../../services/user.service';
+import { ProductService } from '../../services/product.service';
+import { CosmeticResponse } from '../../responses/cosmetic.response';
+import { CartService } from '../../services/cart.service';
+import { CartDTO } from '../../dtos/cart.dto';
+import { LocalStorageService } from '../../services/local-storage.service';
+import { CartResponse } from '../../responses/cart.response';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-cosmetic',
   standalone: true,
-  imports: [CommonModule], // Thêm CommonModule vào đây
+  imports: [
+    CommonModule,
+    FormsModule
+  ], 
   templateUrl: './cosmetic.component.html',
   styleUrls: ['./cosmetic.component.scss']
 })
-export class CosmeticComponent {
-  mainImage: string = 'https://via.placeholder.com/350';
-  images: string[] = [
-    'https://hasaki.vn/images/graphics/img_quality_2.png',
-    'https://media.hcdn.vn/hsk/icons/img_quality_44.png',
-    'https://via.placeholder.com/150/FF0000',
-    'https://via.placeholder.com/150/00FF00'
-  ];
+export class CosmeticComponent implements OnInit {
 
-  changeMainImage(image: string): void {
-    this.mainImage = image;
+  
+  id = input.required<UUID>();
+  cosmetic!: CosmeticResponse;
+
+  mainImageUrl!: string;
+
+  quantity = 1;;
+
+  constructor(
+    private userService : UserService,
+    private productService : ProductService,
+    private cartService : CartService,
+    private localStorageService : LocalStorageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getProductById();
   }
+
+  getProductById() {
+    this.productService.$productById(this.id()).subscribe({
+      next: (cosmetic : CosmeticResponse) => {
+        this.cosmetic = new CosmeticResponse(cosmetic);
+        this.mainImageUrl = this.cosmetic.images[0].url; 
+      },
+      error: (error : any) => {
+        console.error('Error:', error);
+      }
+    });
+  }
+
+  addToCart(){
+    const cartDTO : CartDTO = new CartDTO({
+      user_id : this.localStorageService.get('userId'),
+      product_id : this.cosmetic.id,
+      quantity : this.quantity
+    });
+    console.log(this.quantity);
+    this.cartService.$create(cartDTO).subscribe({
+      next: (cart : CartResponse) => {
+        console.log('Add to cart successfully', cart);
+      },
+      error: (error : any) => {
+        console.error('Error:', error);
+      }
+    });
+  }
+
+  changeMainImage(imageUrl: string): void {
+    this.mainImageUrl = imageUrl;
+  }
+
 }
