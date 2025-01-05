16a337e2091b12ba8be704c1e4062e8b7067198d
NguyenSang <nguyentansangit2411@gmail.com>
2025-01-05 14:02:41 +0700

    header-route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,17 @@ import { AuthComponent } from './components/auth/auth.component';
 import { CartComponent } from './components/cart/cart.component';
 import { authGuard } from './guards/auth.guard';
 import { CosmeticComponent } from './components/cosmetic/cosmetic.component';
+import { SearchComponent } from './components/search/search.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ProductComponent } from './components/admin/product/product.component';
 export const routes: Routes = [
     {path: '', redirectTo: 'homepage', pathMatch: 'full'},
     {path: 'homepage', component: HomepageComponent},
     {path: 'cosmetics/:id', component: CosmeticComponent},
+    {path: 'search', component: SearchComponent},
     {path: 'auth/:type-auth', component: AuthComponent},
+    {path: 'admin', component: AdminComponent},
+    {path: 'product', component: ProductComponent},
     {
         path: 'user',
         canActivateChild: [authGuard],
